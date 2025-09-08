a6655dc97570f2caa978daa0caa0f6a51041a570
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-09-08 10:58:56 +0200

    get operation commit

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const { Board } = require("./models");
+const { Board } = require("../models");
 
-const BoardModel = require("./models").Board;
-const ThreadModel = require("./models").Thread;
-const ReplyModel = require("./models").Reply;
+const BoardModel = require("../models").Board;
+const ThreadModel = require("../models").Thread;
+const ReplyModel = require("../models").Reply;
 
 module.exports = function (app) {
   
@@ -119,10 +119,43 @@ module.exports = function (app) {
               res.send("Suceess!")
             })
           }
-        })
+        });
       })
-    })
+    });
     
-  app.route('/api/replies/:board');
+  app.route('/api/replies/:board').post((req, res) => {
+    console.log("thread", req.body);
+    const { thread_id, text, delete_password } = req.body;
+    const baord = req.params.body;
+    const newReply = new ReplyModel ( {
+      text: text,
+      delete_password: delete_password,
+    });
+    let BoardModel = BoardModel.findOne({ name: board}, (error, boardData) => {
+      if (!boardData) {
+        res.json("error", "Board not found!");
+      } else {
+        const date = new Date();
+        let threadToAddReply = boardData.threads.id(thread_id);
+        threadToAddReply.bumped_on = date;
+        threadToAddReply.replies.push(newReply);
+        boardData.save((error, updateData) => {
+          res.json(updateData);
+        });
+      }
+    });
+  }).get((req, res) => {
+    const board = req.params.board;
+    let BoardModel = BoardModel.findOne({ name: board }, (error, data) => {
+      if (!data) {
+        console.log("No board with this name.");
+        res.json({ error: "No board with this name."});
+      } else {
+        console.log("data", data);
+        const thread = data.thread.id(req.query.thread_id);
+        res.json(thread);
+      }
+    })
+  })
 
 };
